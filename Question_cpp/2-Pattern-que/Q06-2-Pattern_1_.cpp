743502d8c57e8501cf0b7f2afb71cd5e1c155d32
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-03-14 20:10:16 +0530

    pq6

---
+++ Question_cpp/2-Pattern-que/Q06-2-Pattern_1_.cpp
@@ -0,0 +1,28 @@
+//
+//   n=5
+//
+//      *   *   *   *   *
+//              *   *   *   *
+//                      *   *   *    
+//                              *   *
+//                                      *
+
+
+// My Code -->
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of raw to print ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<i;j++){
+            cout<<"        ";
+        }
+        for(int k=1;k<=n-i+1;k++){
+            cout<<"*   ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
