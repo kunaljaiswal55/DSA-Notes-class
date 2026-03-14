a2ae56cf8c65119225975a0b4eb2ea7ca90ce216
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-03-14 20:31:28 +0530

    pq7

---
+++ Question_cpp/2-Pattern-que/Q07-2-Pattern_2_.cpp
@@ -0,0 +1,30 @@
+
+//      for n=5
+//               *                     * * 
+//             * * *                 * * * *
+//           * * * * *             * * * * * *
+//         * * * * * * *         * * * * * * * *
+//       * * * * * * * * *     * * * * * * * * * *
+//          asymmetric         for symmetric one
+
+//my code -->
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of raw to print ";
+    cin>>n;
+    for (int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                cout<<"  ";
+            }
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            for(int j=1;j<=i-1;j++){           //   just do j<=i
+                cout<<"* ";
+            }
+            cout<<endl;
+    }
+    return 0;
+}
