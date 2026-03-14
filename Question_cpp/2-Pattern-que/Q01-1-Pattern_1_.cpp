2b5322c4434d95f38c55da2604cf06eab2c90b2e
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-03-14 19:51:42 +0530

    pq1

---
+++ Question_cpp/2-Pattern-que/Q01-1-Pattern_1_.cpp
@@ -0,0 +1,17 @@
+//
+//  for n=5
+//  * * * * * 
+//
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of start to print ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"* ";
+    }
+    cout<<endl;
+    return 0;
+}
