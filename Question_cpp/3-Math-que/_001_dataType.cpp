544017c7908ab57fc626045a89b815fd85288641
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-03-15 23:07:57 +0530

    q1

---
+++ Question_cpp/3-Math-que/_001_dataType.cpp
@@ -0,0 +1,24 @@
+//Datatype
+#include <iostream>
+using namespace std;
+int main() {
+    byte  a= 127;
+    short b= 32767;
+    int   c= 2147483647;
+    long  d= 923456789l;
+    double  e=3.142857142857142857;
+    float   f= 3.14285714f;
+    bool t=true;
+    char ch='s';
+
+    //output
+    cout<<"byte a= "<<a<<endl;
+    cout<<"short b= "<<b<<endl;
+    cout<<"int c= "<<c<<endl;
+    cout<<"long d= "<<d<<endl;
+    cout<<"double e= "<<e<<endl;
+    cout<<"float f= "<<f<<endl;
+    cout<<"bool t= "<<t<<endl;
+    cout<<"char ch= "<<ch<<endl;
+    return 0;
+}
