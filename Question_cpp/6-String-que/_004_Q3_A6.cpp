068063a08881d7c48a25fe042cdf51e63647148e
Kunal Jaiswal <jaiswalkunal555@gmail.com>
2026-04-01 23:54:10 +0530

    q3

---
+++ Question_cpp/6-String-que/_004_Q3_A6.cpp
@@ -0,0 +1,15 @@
+//question
+//solution
+#include <iostream>
+#include <string>
+#include <vector>
+// using namespace std;
+// bool isCBNumber(long long num) {
+//     if (num == 0 || num == 1) return false;
+//     vector<long long> cbPrimes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+//     for (long long prime : cbPrimes) {
+//         if (num == prime) return true;
+//         if (num % prime == 0) return false;
+//     }
+//     return true;
+// }
